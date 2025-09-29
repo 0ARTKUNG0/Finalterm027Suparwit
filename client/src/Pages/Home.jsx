652738c8710ea265b0f7604c50d30cc4a6fb2adf
0ARTKUNG0 <664259027@webmail.npru.ac.th>
2025-09-29 15:37:28 +0700

    ver4

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../Component/Navbar';
 import Items from '../Component/Item';
-import dotenv from 'dotenv';
 
 const Home = () => {
   const [items, setItems] = useState([]);
